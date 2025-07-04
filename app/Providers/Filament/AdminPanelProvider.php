d4ae5042fdd25ca03de01b43ccfd4bde8338688c
roon <ronald@squint.nl>
2025-07-05 00:53:06 +0200

    SVG

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -19,50 +19,65 @@ use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 use ShuvroRoy\FilamentSpatieLaravelBackup\FilamentSpatieLaravelBackupPlugin;
+use Filament\View\PanelsRenderHook;
+use Filament\Support\Facades\FilamentView;
 
 class AdminPanelProvider extends PanelProvider
 {
-    public function panel(Panel $panel): Panel
-    {
-        return $panel
-			->brandLogo(asset('images/CREA_logo_light.svg'))
-        	->brandLogoHeight('3rem')
-            ->default()
-            ->id('admin')
-            ->path('admin')
-            ->login()
+	public function panel(Panel $panel): Panel
+	{
+		return $panel
+		->brandLogo(asset('images/CREA_logo_light.svg'))
+		->brandLogoHeight('3rem')
+
+		->default()
+		->id('admin')
+		->path('admin')
+		->login()
             ->breadcrumbs(false) // Disable breadcrumbs globally
             ->colors([
-                'primary' => Color::Amber,
+            	'primary' => Color::Amber,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
-                Pages\Dashboard::class,
+            	Pages\Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+            	Widgets\AccountWidget::class,
+            	Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
-                EncryptCookies::class,
-                AddQueuedCookiesToResponse::class,
-                StartSession::class,
-                AuthenticateSession::class,
-                ShareErrorsFromSession::class,
-                VerifyCsrfToken::class,
-                SubstituteBindings::class,
-                DisableBladeIconComponents::class,
-                DispatchServingFilamentEvent::class,
+            	EncryptCookies::class,
+            	AddQueuedCookiesToResponse::class,
+            	StartSession::class,
+            	AuthenticateSession::class,
+            	ShareErrorsFromSession::class,
+            	VerifyCsrfToken::class,
+            	SubstituteBindings::class,
+            	DisableBladeIconComponents::class,
+            	DispatchServingFilamentEvent::class,
             ])
             ->authMiddleware([
-                Authenticate::class,
+            	Authenticate::class,
             ])
 
             ->plugin(FilamentSpatieLaravelBackupPlugin::make()
             	->noTimeout()
-        );
+            );
             
+        }
+
+
+        public function boot(): void
+        {
+        	FilamentView::registerRenderHook(
+        		PanelsRenderHook::GLOBAL_SEARCH_AFTER,
+        		fn (): string => '<a href="https://github.com/Ronald-Ortelee/NEN3140" target="_blank" rel="noopener noreferrer">
+        		<img src="' . asset('images/github-mark.svg') . '" alt="GitHub" class="h-8 w-8 mr-2">
+        		</a>'
+        	);
+        }
+
     }
-}
